f92acc8b8a3c0b71d0525aee5b24f91ee63355b3
linhvu <linh.vup@gmail.com>
2023-01-24 01:06:28 +0000

    added filter for get all user days to only return objects with user id

--- user_days/views.py
+++ user_days/views.py
@@ -3,19 +3,25 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.db import IntegrityError
 from rest_framework.exceptions import NotFound, PermissionDenied
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from .models import UserDay
 from .serializers import UserDaySerializer, PopulatedUserDaySerializer
 
 
 class UserDayListView(APIView):
-    permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAuthenticated, )
 
-    def get(self, _request):
-        user_day = UserDay.objects.all()  # get everything from the shows table in the db
+    def get(self, request):
+        is_staff = request.user.is_staff
+        is_user = request.user.id
+        print("REQUEST USER", request.user.id)
+        if is_staff:
+            user_days = UserDay.objects.all()  # get everything from the shows table in the db
+        else:
+            user_days = UserDay.objects.filter(user=request.user.id)
         # run everything through the serializer
-        serialized_user_days = UserDaySerializer(user_day, many=True)
+        serialized_user_days = UserDaySerializer(user_days, many=True)
         # return the response and a status
         return Response(serialized_user_days.data, status=status.HTTP_200_OK)
 
@@ -26,7 +32,7 @@ class UserDayListView(APIView):
             user_day_to_add.is_valid()
             user_day_to_add.save()
             return Response(user_day_to_add.data, status=status.HTTP_201_CREATED)
-      
+
         except IntegrityError as e:
             res = {
                 "detail": str(e)
@@ -34,20 +40,21 @@ class UserDayListView(APIView):
             return Response(res, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
         except AssertionError as e:
-            return Response({ "detail": str(e) }, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+            return Response({"detail": str(e)}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
         except:
-            return Response({ "detail": "Unprocessable Entity" }, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+            return Response({"detail": "Unprocessable Entity"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
 
 class UserDayDetailView(APIView):
-    permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAuthenticated, )
+
     def get_user_day(self, pk):
         try:
             return UserDay.objects.get(pk=pk)
         except UserDay.DoesNotExist:
             raise NotFound(detail='Cannot find user days')
-    
-    
+
     def get(self, _request, pk):
         try:
             user_day = self.get_user_day(pk=pk)
@@ -61,13 +68,14 @@ class UserDayDetailView(APIView):
         user_day_to_edit = self.get_user_day(pk=pk)
         is_user = request.user.id
         if not is_user:
-          raise PermissionDenied()
-        updated_user_day = UserDaySerializer(user_day_to_edit, data=request.data)
+            raise PermissionDenied()
+        updated_user_day = UserDaySerializer(
+            user_day_to_edit, data=request.data)
         try:
             updated_user_day.is_valid()
             updated_user_day.save()
             return Response(updated_user_day.data, status=status.HTTP_202_ACCEPTED)
-        
+
         except AssertionError as e:
             return Response({"detail": str(e)}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
